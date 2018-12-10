945887f55b00dab744c955210da77d2f3fa45741
ifilandra1 <ifilandra1@etf.unsa.ba>
2018-12-11 00:50:37 +0100

    Dodan button "trazi", te ListView<String> u kojem ce se ispisivati lokacije direktorija u kojima se nalazi podstring

--- Controller.java
+++ Controller.java
@@ -1,27 +1,35 @@
 package ba.unsa.etf.rpr.tutorijal08;
 
 import javafx.beans.property.SimpleStringProperty;
-import javafx.scene.control.TextField;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 
 
 public class Controller {
 
 public TextField unosField;
 public SimpleStringProperty unos;
+public Button trazi;
+public ListView<String> lista;
 
+    public ObservableList<String> datoteke = FXCollections.observableArrayList();
 
     public Controller() {
 
     unosField= new TextField();
     unos=new SimpleStringProperty("");
+
 }
 
-
-public void initialize() {
+    public void initialize() {
 unosField.textProperty().bindBidirectional(unos);
 
 }
 
+
+    public void traziAction(ActionEvent actionEvent) {
+    }
 
 }
